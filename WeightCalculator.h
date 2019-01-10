b35961a6aa7cb7295b9bca8dddd93368647d1c97
Tyler Mitchell <tyler106@ksu.edu>
2019-01-10 17:22:05 -0600

    simplify weight

--- WeightCalculator.h
+++ WeightCalculator.h
@@ -14,31 +14,35 @@
 #include "TRandom3.h"
 #include "TSystem.h"
 #include "TTree.h"
-using namespace std;
+using std::string;
 
-float XSection(std::string OutName) {
-  //    https://docs.google.com/spreadsheets/d/1rWM3AlFKO8IJVaeoQkWZYWwSvicQ1QCXYSzH74QyZqE/edit?alt=json#gid=398123591
+float XSection(string name) {
+  // https://docs.google.com/spreadsheets/d/1rWM3AlFKO8IJVaeoQkWZYWwSvicQ1QCXYSzH74QyZqE/edit?alt=json#gid=398123591
 
-  map<string, float> cross_sections = {
-      {"WJets", 61526},
-      {"DYJetsToLL", 5765.4},
-      {"TTJets", 831.76},
-      {"WW", 115.0},
-      {"WZ", 47.13},
-      {"ZZ", 16.523}};
-
-  return cross_sections[OutName];
+  // I could use a switch, but meh.
+  if (name.find("DYJetsToLL_M-50_Inc") != string::npos) {
+    return 5765.4;
+  } else if (name.find("TTbar") != string::npos) {
+    return 831.76;
+  } else if (name.find("WJetsToLNu_Inc") != string::npos) {
+    return 61526.;
+  } else if (name.find("WW") != string::npos) {
+    return 115.;
+  } else if (name.find("WZ") != string::npos) {
+    return 47.13;
+  } else if (name.find("ZZ") != string::npos) {
+    return 16.523;
+  }
 }
 
-float weightCalc(TH1F *Histo, std::string outputName) {
-
-  if (outputName.find("SingleElectron") != string::npos || outputName.find("SingleMuon") != string::npos) {
+float weightCalc(TH1F *Histo, string name) {
+  if (name.find("SingleElectron") != string::npos || name.find("SingleMuon") != string::npos) {
     return 1.;
   }
 
   float luminosity(35900.);
   if (Histo->GetBinContent(2) > 0) {
-    return luminosity * XSection(outputName) * 1.0 / Histo->GetBinContent(2);
+    return luminosity * XSection(name) * 1.0 / Histo->GetBinContent(2);
   } else {
     cerr << "No content in hEvents" << endl;
     return 0;
